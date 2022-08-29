bf29e71a99a53d2c689c708254d8a30df6cd8d19
Krishna <singhks0054@gmail.com>
2022-08-29 16:52:24 +0530

    firstf

---
+++ components/Hero.tsx
@@ -0,0 +1,46 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/bundle';
+
+// import required modules
+import { Autoplay, Pagination, Navigation, EffectFade } from 'swiper';
+
+export default function App() {
+  return (
+    <>
+      <Swiper
+        centeredSlides={true}
+        autoplay={{
+          delay: 1500,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, EffectFade, Pagination, Navigation]}
+        className='mySwiper'
+      >
+        <SwiperSlide>
+          <img src='https://swiperjs.com/demos/images/nature-1.jpg' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src='https://swiperjs.com/demos/images/nature-2.jpg' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src='https://swiperjs.com/demos/images/nature-3.jpg' />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src='https://swiperjs.com/demos/images/nature-4.jpg' />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+}
