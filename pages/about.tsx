346110fc5d403a9fe85da58a1bd01b1b2a941207
Krishna <singhks0054@gmail.com>
2023-01-16 16:56:23 +0530

    d5

--- pages/about.tsx
+++ pages/about.tsx
@@ -14,13 +14,13 @@ import { staffData } from '../data';
 //   price: string;
 // }[];
 
-export default function about({staffData}) {
+export default function about(props) {
   return (
     <>
       <Header t1={'About Us'} t2={'SUITES HOTEL & RESORT'} />
       <About />
       <section className='grid sm:grid-cols-2 lg:grid-cols-3 container gap-8'>
-        {staffData.map((data, index) => (
+        {props.staffData.map((data, index) => (
           <StaffCard
             key={index}
             img={data?.img}
