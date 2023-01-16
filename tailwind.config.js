ded0e8cf3b457ab3d78471f8bb5e2bf909ad4288
Krishna <singhks0054@gmail.com>
2023-01-16 14:10:58 +0530

    bg updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,9 +22,9 @@ module.exports = {
       },
 
       backgroundImage: {
-        'bg-1': 'url("https://preview.colorlib.com/theme/suites/images/xhero_2.jpg.pagespeed.ic.kt36xQm6lK.webp")',
-        'bg-2': 'url("https://preview.colorlib.com/theme/suites/images/xhero_1.jpg.pagespeed.ic.WnC_CEjW9b.webp")',
-        'bg-3': 'url("https://preview.colorlib.com/theme/suites/images/xhero_3.jpg.pagespeed.ic.0RfTZ9Ub16.webp")',
+        'bg-1': 'https://preview.colorlib.com/theme/suites/images/hero_1.jpg.webp',
+        'bg-2': 'https://preview.colorlib.com/theme/suites/images/hero_2.jpg.webp',
+        'bg-3': 'https://preview.colorlib.com/theme/suites/images/hero_3.jpg.webp',
       }
 
     },
